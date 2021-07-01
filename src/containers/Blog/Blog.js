4394437e782053dd3e00fb106b7f19e8e32e5e41
System Administrator <udin_rajkarnikar2001@yahoo.com>
2021-07-01 02:01:11 -0500

    made each component clickable and displayed it in another component by making asynchronous calls and made some restrictions in the setStateMethod to break the infine loop in componentDidUpdate

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,12 +8,14 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
     
+    //first ma execute huncha before the render method
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
-            const posts = response.data.slice(0, 4)
+            const posts = response.data.slice(0, 4) //print the array from 0 so it only prints from [0] to [3], second argument is 4 i.e. number of elements.
             const updatedPosts = posts.map((eachupdatedPost) => {
                 return{
                     ...eachupdatedPost, //doing this same as doing 'title: eachupdatedPost.title' since at the end we are getting the title in <Post title = {posts.title}/>
@@ -25,9 +27,18 @@ class Blog extends Component {
         })  
     }
 
+    assigningValueInFullPost = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
+
     render () {
         const posts = this.state.posts.map((eachPost) => {
-            return <Post title={eachPost.title} key={eachPost.id} author={eachPost.author}/>
+            return <Post 
+                title={eachPost.title} 
+                key={eachPost.id} 
+                author={eachPost.author} 
+                clicked = {() => this.assigningValueInFullPost(eachPost.id)}/>
         })
         return (
             <div>
@@ -35,7 +46,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
